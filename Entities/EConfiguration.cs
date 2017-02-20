3ba332f5fd5147b7be0564c7d2ce390f592af9b1
final20 <final20@NATALY>
2017-02-19 21:24:18 -0600

    FirstCommit

---
+++ Entities/EConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Entities
+{
+    public class EConfiguration
+    {
+        public string Name => System.IO.Path.GetFileNameWithoutExtension(Path);
+        public string Path { get; set; }
+        public string Content { get; set; } = "";
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(EConfiguration))
+                return false;
+
+            var toCompare = obj as EConfiguration;
+            return toCompare.Path.Equals(Path)
+                   && toCompare.Content.Equals(Content);
+        }
+
+        public override int GetHashCode()
+        {
+            return Path.GetHashCode() ^ Content.GetHashCode();
+        }
+    }
+}
---
+++ Model/FileHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+
+namespace Model
+{
+    public interface IFileHelper
+    {
+        void Copy(string sourceFileName, string destFileName);
+        void Copy(string sourceFileName, string destFileName, bool overwrite);
+        bool Exists(string path);
+        void WriteAllText(string path, string contents);
+        string[] GetFiles(string path);
+        string ReadAllText(string path);
+        void Delete(string path);
+    }
+
+    public class FileHelper : IFileHelper
+    {
+        public void Copy(string sourceFileName, string destFileName)
+        {
+            File.Copy(sourceFileName, destFileName);
+        }
+
+        public void Copy(string sourceFileName, string destFileName, bool overwrite)
+        {
+            File.Copy(sourceFileName, destFileName, overwrite);
+        }
+
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public void WriteAllText(string path, string contents)
+        {
+            File.WriteAllText(path,contents);
+        }
+
+        public string[] GetFiles(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public void Delete(string path)
+        {
+            File.Delete(path);
+        }
+    }
+}
