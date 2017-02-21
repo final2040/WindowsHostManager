ba1d8e0abfbc2ce4dd7b73fcab4ad877a23772ea
final20 <final20@NATALY>
2017-02-20 21:25:39 -0600

    sad

---
+++ UnitTests/FileManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture()]
+    public class FileManagerTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var configuration = new EConfiguration();
+        }
+    }
+}
